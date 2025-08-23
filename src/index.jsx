73fb3b0db3bcbe5ad04defd44ff10463e80638d4
rockandbit <rockandbit@gmail.com>
2025-08-23 09:49:34 +0200

    feat: add netlify-identity-widget for user authentication integration

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,9 @@ import App from "./App.jsx";
 import { HelmetProvider } from "react-helmet-async";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import netlifyIdentity from "netlify-identity-widget";
+
+netlifyIdentity.init();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HelmetProvider>
